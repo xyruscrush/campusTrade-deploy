1c31a131bd2ed67db1fe2af089dd6e1c0ea94565
xyrus <xyruscrush1234567890@gmail.com>
2026-07-14 18:12:18 +0000

    config: explicitly connect to the 'trade' database in mongoose

--- campustrade-main/db.js
+++ campustrade-main/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "trade" });
     console.log("MongoDB Connected...");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
